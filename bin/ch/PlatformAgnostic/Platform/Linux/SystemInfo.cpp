ebb9a51f8666f7c272cf7e79ac0e335f8549e18e
Hitesh Kanwathirtha <hiteshk@microsoft.com>
2017-10-28 04:50:46 +0000

    Additional cleanup

--- bin/ch/PlatformAgnostic/Platform/Linux/SystemInfo.cpp
+++ bin/ch/PlatformAgnostic/Platform/Linux/SystemInfo.cpp
@@ -3,11 +3,26 @@
 // Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
 //-------------------------------------------------------------------------------------------------------
 
-#ifdef __linux__
+#include <stdafx.h>
 #include <ChPlatformAgnostic.h>
-#include <sys/resource.h>
+#include <unistd.h> // readlink
+#include <limits.h>
 
 namespace ChPlatformAgnostic
 {
+    bool Module::GetBinaryLocation(char* const path, const charcount_t size, charcount_t* const resultStrLength)
+    {
+        if (path == nullptr || resultStrLength == nullptr || size > UINT_MAX) return false;
+
+        int str_len = readlink("/proc/self/exe", path, size - 1);
+        if (str_len <= 0)
+        {
+            strcpy_s(path, size,  "GetBinaryLocation: /proc/self/exe has failed.");
+            return false;
+        }
+        path[str_len] = char(0);
+
+        *resultStrLength = str_len;
+        return true;
+    }
 }
-#endif
--- bin/ch/PlatformAgnostic/Platform/Unix/SystemInfo.cpp
+++ bin/ch/PlatformAgnostic/Platform/Unix/SystemInfo.cpp
@@ -3,9 +3,28 @@
 // Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
 //-------------------------------------------------------------------------------------------------------
 
-#include "Common.h"
-#include "ChakraPlatform.h"
+#include <stdafx.h>
+#include <ChPlatformAgnostic.h>
+#include <mach-o/dyld.h> // _NSGetExecutablePath
 
 namespace PlatformAgnostic
 {
+    bool Module::GetBinaryLocation(char* const path, const charcount_t size, charcount_t* const resultStrLength)
+    {
+        if (path == nullptr || resultStrLength == nullptr || size > UINT_MAX) return false;
+
+        char executablePath[PATH_MAX]
+        uint32_t path_size = _countof(executablePath);
+        if (_NSGetExecutablePath(executablePath, &path_size))
+        {
+            strcpy_s(path, size, "GetBinaryLocation: _NSGetExecutablePath has failed.");
+            return false;
+        }
+
+        char *result = realpath(executablePath, path);
+
+        *resultStrLength = strlen(result);
+        
+        return true;
+    }
 }
